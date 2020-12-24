f6c1a26906806b321b0a5a1747a3388cde4ff293
Elena Kardash <infongentest1@gmail.com>
2020-12-25 02:26:47 +0300

    Add test for Task6

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,12 +1,12 @@
 package day1;
 
 public class Task6 {
+    public static int k = 5;
     public static void main(String[] args) {
-        int k = 5;
 
         for (int i = 1; i < 10; i++) {
-            System.out.println(i + " x " + k + " = " + (i * k));
+            System.out.println(i + " x " + Task6.k + " = " + (i * Task6.k));
         }
-       
+
     }
 }
--- src/test/java/Tests/day1/Day1Test.java
+++ src/test/java/Tests/day1/Day1Test.java
@@ -6,6 +6,7 @@ import day1.Task2;
 import day1.Task3;
 import day1.Task4;
 import day1.Task5;
+import day1.Task6;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -77,5 +78,42 @@ public class Day1Test extends AbstractTest {
         Task5.main(new String[0]);
         assertEquals(resultString, getOutput(), "Не верное количество строк или ошибка в тексте");
     }
+    @Test
+    public void Task6() {
+
+        Integer value = Task6.k;
+        int counter = 1;
+
+        final String resultString1 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString2 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString3 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString4 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString5 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString6 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString7 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString8 = counter + " x " + value.toString() + " = " + (counter * value);
+        counter++;
+        final String resultString9 = counter + " x " + value.toString() + " = " + (counter * value);
+
+        Task6.main(new String[0]);
+        String resultString = resultString1 + System.lineSeparator() +
+                resultString2 + System.lineSeparator()+
+                resultString3 + System.lineSeparator()+
+                resultString4 + System.lineSeparator()+
+                resultString5 + System.lineSeparator()+
+                resultString6 + System.lineSeparator()+
+                resultString7 + System.lineSeparator()+
+                resultString8 + System.lineSeparator()+
+                resultString9 + System.lineSeparator();
+        assertEquals(resultString, getOutput(), "Не верное количество строк или ошибка в тексте");
+    }
+
 
 }
