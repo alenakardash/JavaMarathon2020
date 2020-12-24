f7447efbbfdef9da7a1ba3d3808e70c5f1ec3c63
Elena Kardash <infongentest1@gmail.com>
2020-12-24 19:18:47 +0300

    Add Day 1 tasks solutions

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,8 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        for (int i = 10; i > 0; i--) {
+            System.out.println("JAVA");
+        }
     }
 }
