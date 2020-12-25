9aed40900c7f4a919c0551f0f0d15fcf5fab1cc3
Elena Kardash <infongentest1@gmail.com>
2020-12-25 14:13:01 +0300

    Add day2 tasks solutions

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//      System.out.println("Укажите количество этажей в здании");
+
+        int floorNumber = sc.nextInt();
+
+        if (floorNumber >= 1 && floorNumber <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (floorNumber >= 5 && floorNumber <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (floorNumber >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
 
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,27 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
+
+        int value = a + 1;
+        while (value < b) {
+            if (value % 5 == 0 && value % 10 != 0) {
+                System.out.print(value + " ");
+            }
+            value++;
+        }
     }
+
 }
