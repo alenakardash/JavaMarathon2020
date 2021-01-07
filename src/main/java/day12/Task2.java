45eb376e7993a8b8b461329b39ac3a0dce6770ab
Elena Kardash <infongentest1@gmail.com>
2021-01-07 12:58:34 +0300

    Corrections to day12 tasks

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -7,14 +7,8 @@ public class Task2 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 31; i++) {
-            if (i % 2 == 0) {
-                list.add(i);
-            }
-        }
-
-        for (int i = 300; i < 351; i++) {
-            if (i % 2 == 0) {
+        for (int i = 0; i < 351; i++) {
+            if (i % 2 == 0 && (i < 31 || i >= 300)) {
                 list.add(i);
             }
         }
