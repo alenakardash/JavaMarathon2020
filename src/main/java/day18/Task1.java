0c32b664abc58f3d176f43c16a35c658fcb8d52b
Elena Kardash <infongentest1@gmail.com>
2021-01-12 10:05:51 +0300

    Add day18 tasks solutions

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,17 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] num = {1, 1, 1, 1, 5};
+
+        System.out.println(recursionSum(num, 0));
+    }
+
+    public static int recursionSum(int[] mas, int n) {
+        if (n == mas.length - 1) {
+            return mas[mas.length - 1];
+        } else {
+            return mas[n] + recursionSum(mas, n + 1);
+        }
 
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,20 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(count7(12727767));
 
+    }
+
+    public static int count7(int number) {
+        if (Math.abs(number) < 1) {
+            return 0;
+        } else {
+            int temp = number % 10;
+            if (temp == 7) {
+                return 1 + count7(number / 10);
+            } else {
+                return count7(number / 10);
+            }
+        }
     }
 }
